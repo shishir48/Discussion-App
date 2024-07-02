d06a17c743f29a91b1607356e34a92d5bdb35632
shishir singh <shishir.singh@eisenvault.com>
2024-07-02 20:07:29 +0530

    login endpoint added

---
+++ user-service/src/controllers/users/login/login.controller.js
@@ -0,0 +1,24 @@
+const User = require("../../../models/user/user.model")
+const generateToken = require("../../../utils/generateToken")
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ email })
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        mobile: user.mobile,
+        token: generateToken(user._id),
+      })
+    } else {
+      res.status(401).json({ message: "Invalid email or password" })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
--- user-service/src/routes/routes.js
+++ user-service/src/routes/routes.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const {
   register,
 } = require("../controllers/users/register/register.controller")
+const {login} = require("../controllers/users/login/login.controller")
 
 router.post("/register", register)
+router.post("/login", login)
 
 module.exports = router
